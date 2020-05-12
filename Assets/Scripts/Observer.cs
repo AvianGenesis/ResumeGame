83bbbafbfce06ec0c778a413756a3acc882aaaf7
zflaherty <zflahert@trinity.edu>
2020-05-12 15:46:09 -0600

    Portfolio button updates info pane. Formatted portfolio scroll view

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Observer : MonoBehaviour
+{
+    public Text summary;
+    public Text fullInfo;
+    public Image preview;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateInfo(string summ, string fInfo, Sprite prev)
+    {
+        summary.text = summ;
+        //fullInfo.text = fInfo;
+        preview.sprite = prev;
+    }
+}
---
+++ Assets/Scripts/PortBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortBtn : MonoBehaviour
+{
+    private Observer obs;
+
+    public string summary;
+    public string fullInfo;
+    public Sprite gamePreview;
+
+    public void Start()
+    {
+        obs = GameObject.Find("Main Camera").GetComponent<Observer>();
+    }
+
+    public void UpdateInfo()
+    {
+        obs.UpdateInfo(summary, fullInfo, gamePreview);
+    }
+}
