8f65952ffe802ffe871572b300d38dd0bceee7e4
zflaherty <zflahert@trinity.edu>
2020-05-25 17:59:56 -0600

    Move along ground, can only move allotted space count

--- Assets/Scripts/GridMovement/GridPlayerControl.cs
+++ Assets/Scripts/GridMovement/GridPlayerControl.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class GridPlayerControl : MonoBehaviour
 {
+    public int moveCount;
+
     private float step;
     private bool canMove;
     private bool isMoving;
     private Vector3 start;
     private Vector3 end;
     private GameObject nextSpace;
+    private Stack<GameObject> spaceStack;
+    private Rigidbody rb;
     private Transform body;
+    private RaycastHit hit;
     private GridSpace curGS;
     [SerializeField] private GameObject curSpace;
 
@@ -19,18 +24,22 @@ public class GridPlayerControl : MonoBehaviour
     {
         canMove = true;
         isMoving = false;
+        spaceStack = new Stack<GameObject>();
+        rb = GetComponent<Rigidbody>();
         body = transform.GetChild(0);
         transform.position = curSpace.transform.position + new Vector3(0.0f, 1.1f, 0.0f);
         curGS = curSpace.GetComponent<GridSpace>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (isMoving)
         {
             step += Time.deltaTime * 20.0f;
-            transform.position = Vector3.MoveTowards(start, end, step);
-            if(Vector3.Distance(transform.position, end) <= 0.01f)
+            transform.position = Vector3.MoveTowards(new Vector3(start.x, transform.position.y, start.z), new Vector3(end.x, transform.position.y, end.z), step);
+            //Debug.Log("Pos: " + transform.position);
+            //Debug.Log("End: " + end);
+            if (Mathf.Abs(transform.position.x - end.x) <= 0.01f && Mathf.Abs(transform.position.z - end.z) <= 0.01f)
             {
                 isMoving = false;
                 step = 0.0f;
@@ -42,47 +51,70 @@ public class GridPlayerControl : MonoBehaviour
         }
     }
 
-    private void SetMoving()
+    /* Keep player grounded */
+    private void Update()
+    {
+        if (Physics.Raycast(new Ray(transform.position, Vector3.down), out hit))
+        {
+            transform.Translate(new Vector3(0.0f, -hit.distance + 1.1f, 0.0f));
+        }
+    }
+
+    /* Trigger movement */
+    private void SetMoving(GameObject next)
     {
         isMoving = true;
+        nextSpace = next;
         start = curSpace.transform.position + new Vector3(0.0f, 1.1f, 0.0f);
         end = nextSpace.transform.position + new Vector3(0.0f, 1.1f, 0.0f);
         body.LookAt(end);
     }
 
+    /* Monitor move count and direction */
+    private void CheckCount(GameObject next)
+    {
+        if (!isMoving && next != null)
+        {
+            if (spaceStack.Count > 0)
+            {
+                if (next == spaceStack.Peek())
+                {
+                    moveCount++;
+                    spaceStack.Pop();
+                    SetMoving(next);
+                    return;
+                }
+            }
+            if (moveCount > 0)
+            {
+                moveCount--;
+                spaceStack.Push(curSpace);
+                SetMoving(next);
+            }
+        }
+    }
+
+
+    /*********/
+    /* Input */
+    /*********/
     void OnUp()
     {
-        if(curGS.north != null && !isMoving)
-        {
-            nextSpace = curGS.north;
-            SetMoving();
-        }
+        CheckCount(curGS.north);
     }
 
     void OnDown()
     {
-        if (curGS.south != null && !isMoving)
-        {
-            nextSpace = curGS.south;
-            SetMoving();
-        }
+        CheckCount(curGS.south);
     }
 
     void OnLeft()
     {
-        if (curGS.west != null && !isMoving)
-        {
-            nextSpace = curGS.west;
-            SetMoving();
-        }
+        CheckCount(curGS.west);
     }
 
     void OnRight()
     {
-        if (curGS.east != null && !isMoving)
-        {
-            nextSpace = curGS.east;
-            SetMoving();
-        }
+        CheckCount(curGS.east);
     }
 }
