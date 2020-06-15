2643dd56b6c64f9e7b00b9302c1a3a95ca7678ab
zflaherty <zflahert@trinity.edu>
2020-06-15 00:13:56 -0600

    Drew idle and ground1, can walk right

---
+++ Assets/Scripts/MarioLevel/MLPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLPlayer : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRight()
+    {
+        Debug.Log("Right");
+        rb.AddForce(new Vector2(100.0f, 0.0f));
+    }
+
+    void OnLeft()
+    {
+
+    }
+
+    void OnUp()
+    {
+        
+    }
+}
